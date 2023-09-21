e23b489c1885897556000d143ef98d27646b2556
Ruth Balbina Figueredo <ruth_lestat@hotmail.com>
2023-09-21 20:15:52 -0300

    segundo ejercicio terminado

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const welcome = (nombre) => {
 
 console.log(welcome(nombre))
 
-//Ejercicio 2:
+//Ejercicio 1:
 //Crear una funcion que reciba como parámetro dos numeros y devuelva el resultado de cada calculo aritmético:
 const disminucion = (numero1, numero2) => {
     const restar = (numero1 - numero2)
@@ -28,5 +28,18 @@ const producto = (numero1, numero2) =>  {
     const multiplicar = (numero1 * numero2)
     return multiplicar
 }
+
+//Ejercicio 2:
+//Crea una funcion que recibe como parametro un sueldo y un numero que representa el porcentaje de aumento. La funcion debe devolver el sueldo mas el aumento.
+const sueldo = Number(prompt("Coloca tu sueldo actual:"))
+const porcAumento = Number(prompt("Coloca tu porcentaje de aumento por paritaria:")) 
+
+const aumentoDeSueldo = (sueldo, porcAumento) =>    {
+    const sueldoNeto = (((sueldo*porcAumento)/100) + sueldo)
+    return sueldoNeto
+}
+
+console.log((aumentoDeSueldo(sueldo, porcAumento)))
+     
 
 
